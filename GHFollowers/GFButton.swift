780251dcb0503e272a5a6bc7cbabe148b74b58ce
nssina <rabie_sina@yahoo.com>
2020-12-03 11:06:25 -0500

    Create GFButton

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Sina Rabiei on 12/3/20.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+ 
