84004cc006eeca5f73d696539b836d2a316b5d73
nssina <rabie_sina@yahoo.com>
2020-12-04 11:39:03 -0500

    Created model

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Sina Rabiei on 12/4/20.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GHFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  Users.swift
+//  GHFollowers
+//
+//  Created by Sina Rabiei on 12/4/20.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
