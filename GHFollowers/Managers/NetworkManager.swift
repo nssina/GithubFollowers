dec8559c54364c9af07dbba578c80dbdd19dfea9
nssina <rabie_sina@yahoo.com>
2020-12-05 10:47:40 -0500

    Download and cache avatars

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Sina Rabiei on 12/4/20.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
