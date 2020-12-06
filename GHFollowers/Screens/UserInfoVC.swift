1f817e939c49b566d054270baff0434ee852765a
nssina <rabie_sina@yahoo.com>
2020-12-06 09:09:32 -0500

    Finish header view

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -19,18 +19,18 @@ class UserInfoVC: UIViewController {
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dimissVC))
         navigationItem.rightBarButtonItem = doneButton
         
+        layoutUI()
+        
         NetworkManager.shared.getUserInfo(for: username) { [weak self] (result) in
             guard let self = self else { return }
             
             switch result {
             case .success(let user):
-                print(user)
+                DispatchQueue.main.async { self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView) }
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "OK")
             }
         }
-        
-        layoutUI()
     }
     
     
@@ -38,14 +38,20 @@ class UserInfoVC: UIViewController {
         view.addSubview(headerView)
         headerView.translatesAutoresizingMaskIntoConstraints = false
         
-        headerView.backgroundColor = .systemPink
-        
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             headerView.heightAnchor.constraint(equalToConstant: 180)
         ])
+    }
+    
+    
+    func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
     }
     
     @objc func dimissVC() {
