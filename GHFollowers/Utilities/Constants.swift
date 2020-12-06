d2c7f853d4d6ce9844142c0a0e2f6217910ede61
nssina <rabie_sina@yahoo.com>
2020-12-06 08:57:31 -0500

    Configured header info

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Sina Rabiei on 12/6/20.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
---
+++ GHFollowers/Utilities/GFError.swift
@@ -0,0 +1,15 @@
+//
+//  GFError.swift
+//  GHFollowers
+//
+//  Created by Sina Rabiei on 12/6/20.
+//
+
+import Foundation
+
+enum GFError: String, Error {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
