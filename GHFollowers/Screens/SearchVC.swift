782d44a68c8083bb2c66febb4232e0ead2b5e0b6
nssina <rabie_sina@yahoo.com>
2020-12-09 10:52:54 -0500

    Fix some UI bugs

--- GHFollowers/Screens/SearchVC.swift
+++ GHFollowers/Screens/SearchVC.swift
@@ -29,6 +29,7 @@ class SearchVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        usernameTextField.text = ""
         navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
@@ -37,6 +38,8 @@ class SearchVC: UIViewController {
             presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to look for 😀.", buttonTitle: "OK")
             return
         }
+        
+        usernameTextField.resignFirstResponder()
         
         let followerListVC = FollowerListVC()
         followerListVC.username = usernameTextField.text
