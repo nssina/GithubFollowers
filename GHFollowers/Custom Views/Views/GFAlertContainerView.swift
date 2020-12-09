04a10d05423cabb3c8452d612baa256852bc8aa8
nssina <rabie_sina@yahoo.com>
2020-12-09 11:12:50 -0500

    Refactoring alert view

---
+++ GHFollowers/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,30 @@
+//
+//  GFAlertContainerView.swift
+//  GHFollowers
+//
+//  Created by Sina Rabiei on 12/9/20.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+    }
+}
